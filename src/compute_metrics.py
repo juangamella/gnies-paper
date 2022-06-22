75103af548bb5e3d175b2c049be06ab8aeb85626
Juan L. Gamella <juan.gamella@stat.math.ethz.ch>
2022-06-22 16:38:01 +0200

    First iteration of compute metrics; ongoing

--- src/compute_metrics.py
+++ src/compute_metrics.py
@@ -36,10 +36,12 @@ import sempler
 import sempler.generators as gen
 import numpy as np
 import src.utils as utils
+import gnies.utils
 import gc  # Garbage collector
 import time
 import os
 import pickle
+import src.metrics as metrics
 
 # --------------------------------------------------------------------
 # Parse input parameters
@@ -76,4 +78,64 @@ print(args)  # For debugging
 # --------------------------------------------------------------------
 # Compute metrics
 
+use_metrics = [metrics.type_1_structc, metrics.type_2_structc]
 methods = args.methods.split(',')
+
+# Extract dataset information
+info = utils.read_pickle(args.directory + utils.INFO_FILENAME)
+n_cases, runs, Ns = info['n_cases'], info['runs'], info['Ns']
+Ns_idx = dict(zip(sorted(Ns), range(len(Ns))))
+
+# Build ground truths
+print("---------------------------------------------")
+print("Building ground truths\n")
+size = (len(Ns), n_cases, runs)
+ground_truth = np.empty(n_cases, dtype=object)
+for i, (scm, interventions) in enumerate(info['cases']):
+    print("  case %d" % i)
+    print("    interventions :", interventions)
+    union = set()
+    for intervention in interventions:
+        parameters = list(intervention.values())[0]
+        union |= set(list(parameters.keys()))
+    print("    union of targets :", union)
+    true_class = gnies.utils.imec(scm.W, union)
+    print("    %d DAGs in the true equiv. class" % len(true_class))
+    ground_truth[i] = true_class
+    print("    done.\n")
+
+#assert not np.any(ground_truth == None)
+
+# Build arrays to store metrics
+shape = (len(Ns), n_cases, runs)
+metrics = {}
+for metric in use_metrics:
+    metrics[metric] = dict(zip(methods, [np.zeros(shape, dtype=float)] * len(methods)))
+
+
+iterable = gnies.utils.cartesian([Ns, range(n_cases), range(runs)], dtype=int)
+print("---------------------------------------------")
+for method in methods:
+    print("Computing metrics for method = %s" % method)
+    results = utils.read_pickle(args.directory + utils.compiled_results_filename(method))
+    estimates = results['estimates']
+    # Build estimated classes
+    print("  building estimated classes")
+    estimated_classes = np.empty(shape, dtype=object)
+    for (n, case, run) in iterable:
+        i = Ns_idx[n]
+        estimated_classes[i, case, run] = gnies.utils.all_dags(estimates[i, case, run])
+    # Compute metrics
+    print("  computing metrics")
+    for metric in use_metrics:
+        print("    metric :", metric)
+        print("      ", end="")
+        for (n, case, run) in iterable:
+            print(".", end="")
+            i = Ns_idx[n]
+            # print("   ", (i, case, run))
+            metrics[metric][method][i, case, run] = metric(
+                estimated_classes[i, case, run], ground_truth[case])
+        print(" done.")
+
+# print(metrics)
