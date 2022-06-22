46323eacf2a7f04f0af83bc98f69e9b074eb3cb4
Juan L. Gamella <juan.gamella@stat.math.ethz.ch>
2022-06-22 15:20:21 +0200

    Script to compute metrics on a set of estimates; ongoing

---
+++ src/compute_metrics.py
@@ -0,0 +1,79 @@
+# Copyright 2020 Juan Luis Gamella Martin
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+
+# 3. Neither the name of the copyright holder nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+"""
+"""
+
+import argparse
+import sempler
+import sempler.generators as gen
+import numpy as np
+import src.utils as utils
+import gc  # Garbage collector
+import time
+import os
+import pickle
+
+# --------------------------------------------------------------------
+# Parse input parameters
+
+# Definitions and default settings
+arguments = {
+    # Execution parameters
+    'directory': {'type': str},
+    'n_workers': {'default': 1, 'type': int},
+    'debug': {'default': False, 'type': bool},
+    'chunksize': {'type': int, 'default': 1},
+    # Other flags parameters
+    'methods': {'type': str},
+}
+
+# Parse settings from input
+parser = argparse.ArgumentParser(description='Run experiments')
+for name, params in arguments.items():
+    if params['type'] == bool:
+        options = {'action': 'store_true'}
+    else:
+        options = {'action': 'store', 'type': params['type']}
+    if 'default' in params:
+        options['default'] = params['default']
+    parser.add_argument("--" + name,
+                        dest=name,
+                        required=False,
+                        **options)
+
+args = parser.parse_args()
+print(args)  # For debugging
+
+
+# --------------------------------------------------------------------
+# Compute metrics
+
+methods = args.methods.split(',')
