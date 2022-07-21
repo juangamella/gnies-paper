9b9134401381f13f9aa6c88bd2ffa52a5eaafbd5
Juan L. Gamella <juan.gamella@stat.math.ethz.ch>
2022-07-21 18:09:06 +0200

    Fixed bug in script

--- copy_euler_results.sh
+++ copy_euler_results.sh
@@ -1,1 +1,7 @@
-scp gajuan@euler.ethz.ch:"$1{test_cases,compiled_results_ges,compiled_results_gies,compiled_results_gnies,compiled_results_ut_igsp,compiled_results_utigsp_plus}.pickle" "$2"
+if [[ $# -eq 1 ]]; then
+   mkdir $1
+   scp gajuan@euler.ethz.ch:"/cluster/scratch/gajuan/$1{test_cases,compiled_results_ges,compiled_results_gies,compiled_results_gnies,compiled_results_ut_igsp,compiled_results_ut_igsp_plus}.pickle" $1
+else
+    mkdir $2
+    scp gajuan@euler.ethz.ch:"$1{test_cases,compiled_results_ges,compiled_results_gies,compiled_results_gnies,compiled_results_ut_igsp,compiled_results_ut_igsp_plus}.pickle" "$2"
+fi
