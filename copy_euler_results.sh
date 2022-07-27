70b52a6ae076e8602493fdf8a1fb626728b9e36b
Juan L. Gamella <juan.gamella@stat.math.ethz.ch>
2022-07-27 16:03:34 +0200

    Updated euler copy script

--- copy_euler_results.sh
+++ copy_euler_results.sh
@@ -1,7 +1,8 @@
+FILENAMES="{test_cases.pickle,compiled_results*}"
 if [[ $# -eq 1 ]]; then
    mkdir $1
-   scp gajuan@euler.ethz.ch:"/cluster/scratch/gajuan/$1{test_cases,compiled_results_ges,compiled_results_gies,compiled_results_gnies,compiled_results_ut_igsp,compiled_results_ut_igsp_plus}.pickle" $1
+   scp gajuan@euler.ethz.ch:"/cluster/scratch/gajuan/$1$FILENAMES" $1
 else
     mkdir $2
-    scp gajuan@euler.ethz.ch:"$1{test_cases,compiled_results_ges,compiled_results_gies,compiled_results_gnies,compiled_results_ut_igsp,compiled_results_ut_igsp_plus}.pickle" "$2"
+    scp gajuan@euler.ethz.ch:"$1$FILENAMES" "$2"
 fi
